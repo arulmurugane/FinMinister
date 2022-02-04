2078e178dfc22bc41dda8735ea1d58b3e29e3339
arulmurugan <arul.tev@gmail.com>
2022-02-04 13:10:25 +0530

    Added Unitest project

---
+++ FInMinister.Applicaiton.UniTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FInMinister.Applicaiton.UniTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ FinMinister.Applicaiton.UnitTests/Expenses/CreateExpenseTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinMinister.Applicaiton.UnitTests.Expenses
+{
+    internal class CreateExpenseTests
+    {
+    }
+}
